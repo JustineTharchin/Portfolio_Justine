35a8268f1fc9ba370ecdac0de23f9a341d63aa3f
Justine Tharchin <54324028+JustineTharchin@users.noreply.github.com>
2024-03-20 15:05:57 +0100

    Add files via upload

---
+++ images/photographies.js
@@ -0,0 +1,64 @@
+var imageData = {
+    "sections": [
+      {
+        "images": [
+          { "src": "./INDIA4.jpg", "alt": "Big Ben" },
+          { "src": "./IMG_0768.JPG", "alt": "Eiffel Tower" },
+          { "src": "./INDIA1.jpg", "alt": "Colosseum" },
+          { "src": "./INDIA2.jpg", "alt": "Pisa Tower" },
+          { "src": "./INDIA3.jpg", "alt": "" },
+          { "src": "./IMG_0934.jpg", "alt": "Sydney Opera House" }
+        ]
+      },
+      {
+        "images": [
+          { "src": "./11.jpg", "alt": "Big Ben" },
+          { "src": "./6.jpg", "alt": "Eiffel Tower" },
+          { "src": "./12.jpg", "alt": "Colosseum" },
+          { "src": "./3.jpg", "alt": "Pisa Tower" },
+          { "src": "./5.jpg", "alt": "" },
+          { "src": "./16.jpg", "alt": "Sydney Opera House" }
+        ]
+      },
+      {
+        "images": [
+          { "src": "./ju.jpg", "alt": "Big Ben" },
+          { "src": "./melissa.jpg", "alt": "Eiffel Tower" },
+          { "src": "./deb1.jpg", "alt": "Colosseum" },
+          { "src": "./joooooo.jpg", "alt": "Pisa Tower" },
+          { "src": "./deb.jpg", "alt": "" },
+          { "src": "./emy2.jpg", "alt": "Sydney Opera House" }
+        ]
+      },
+      {
+        "images": [
+          { "src": "./ju.jpg", "alt": "Big Ben" },
+          { "src": "./emy.jpg", "alt": "Eiffel Tower" },
+          { "src": "./moon.jpg", "alt": "Colosseum" },
+          { "src": "./OREL.jpg", "alt": "Pisa Tower" },
+          { "src": "./noiretblanc.jpg", "alt": "" },
+          { "src": "./emy2.jpg", "alt": "Sydney Opera House" }
+        ]
+      },
+      {
+        "images": [
+          { "src": "./IMG_17708CR2.jpg", "alt": "Big Ben" },
+          { "src": "./IMG_1038.CR2.jpg", "alt": "Eiffel Tower" },
+          { "src": "./IMG_1014.CR2.jpg", "alt": "Colosseum" },
+          { "src": "./IMG_1717.jpg", "alt": "Pisa Tower" },
+          { "src": "./1.jpg", "alt": "" },
+          { "src": "./billyIdol.JPG", "alt": "Sydney Opera House" }
+        ]
+      },
+      {
+        "images": [
+          { "src": "./IMG_2193.jpg", "alt": "Big Ben" },
+          { "src": "./IMG_17338CR2.jpg", "alt": "Eiffel Tower" },
+          { "src": "./P1020523.jpg", "alt": "Colosseum" },
+          { "src": "./18.jpg", "alt": "Pisa Tower" },
+          { "src": "./3.jpg", "alt": "" },
+          { "src": "./9.JPG", "alt": "Sydney Opera House" }
+        ]
+      }
+    ]
+  }
